16352f10c0c20c92d8bbfe8f4913e801391f3621
eremeevfd <dartfedor@mail.ru>
2016-11-01 10:03:00 +0300

    Ended script, made 2 functions, added comments

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,22 @@
+import os
+import collections
+import re
 
-
+# Opens file and reads to String
 def load_data(filepath):
-    pass
+    if os.path.exists(filepath):
+        with open(filepath, 'r',encoding="windows-1251") as my_file:
+            return my_file.read().replace('\n', '').lower()
 
-
+# Finds only words in text using regular expression
+# Gets 10 most frequen words using Counter
 def get_most_frequent_words(text):
-    pass
+    all_found_words = re.findall('\w+',text)
+    return collections.Counter(all_found_words).most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    opened_file = load_data(input("Enter path to file: "))
+    ten_most_frequent_words = get_most_frequent_words(opened_file)
+    for word in ten_most_frequent_words:
+        print("Word: {0} Frequency: {1}".format(word[0].ljust(5), word[1]))
