581960dce5f3f79ea415f04006f82b08d0e7e5b3
eremeevfd <dartfedor@mail.ru>
2016-11-26 13:33:02 +0300

    removed magic constant to var, deleted comments

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,17 +2,17 @@ import os
 import collections
 import re
 
-# Opens file and reads to String
+frequent_words_number = 10
+
 def load_data(filepath):
     if os.path.exists(filepath):
         with open(filepath, 'r',encoding="windows-1251") as my_file:
             return my_file.read().replace('\n', '').lower()
 
-# Finds only words in text using regular expression
-# Gets 10 most frequen words using Counter
+
 def get_most_frequent_words(text):
     all_found_words = re.findall('\w+',text)
-    return collections.Counter(all_found_words).most_common(10)
+    return collections.Counter(all_found_words).most_common(frequent_words_number)
 
 
 if __name__ == '__main__':
