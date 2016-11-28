9ebc316d9fe2de88a5432dd662da756c7f3e1956
eremeevfd <dartfedor@mail.ru>
2016-11-28 11:12:22 +0300

    fixed text loading and magic constant

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,15 +2,14 @@ import os
 import collections
 import re
 
-frequent_words_number = 10
-
 def load_data(filepath):
     if os.path.exists(filepath):
-        with open(filepath, 'r',encoding="windows-1251") as my_file:
-            return my_file.read().replace('\n', '').lower()
+        with open(filepath, 'r') as my_file:
+            return my_file.read().replace('\n', ' ').lower()
 
 
 def get_most_frequent_words(text):
+    frequent_words_number = int(input("How many frequent words do you need? "))
     all_found_words = re.findall('\w+',text)
     return collections.Counter(all_found_words).most_common(frequent_words_number)
 
