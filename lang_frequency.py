2733e944601543d6a97dcd84cbea84acd1145803
eremeevfd <dartfedor@mail.ru>
2016-11-30 11:44:58 +0300

    fixed frequency function to be pure

--- lang_frequency.py
+++ lang_frequency.py
@@ -8,14 +8,14 @@ def load_data(filepath):
             return my_file.read().replace('\n', ' ').lower()
 
 
-def get_most_frequent_words(text):
-    frequent_words_number = int(input("How many frequent words do you need? "))
+def get_most_frequent_words(text, frequent_words_number=10):
     all_found_words = re.findall('\w+',text)
     return collections.Counter(all_found_words).most_common(frequent_words_number)
 
 
 if __name__ == '__main__':
     opened_file = load_data(input("Enter path to file: "))
-    ten_most_frequent_words = get_most_frequent_words(opened_file)
+    frequent_words_number = int(input("How many frequent words do you need? "))
+    ten_most_frequent_words = get_most_frequent_words(opened_file, frequent_words_number)
     for word in ten_most_frequent_words:
         print("Word: {0} Frequency: {1}".format(word[0].ljust(5), word[1]))
